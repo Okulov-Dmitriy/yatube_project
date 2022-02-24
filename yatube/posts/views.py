89b046b6ac658a91cdd7aeb3605c3b481e6d6579
Дмитрий Окулов <okulov.d@mail.ru>
2022-02-24 22:10:39 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+# Главная страница
+def index(request):
+    return HttpResponse('Главная')
+
+def gruop_posts(request, slug):
+    return HttpResponse(f'Посты, отфильтрованные по группе {slug}')
+
