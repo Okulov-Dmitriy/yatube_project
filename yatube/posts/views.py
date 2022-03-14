2a607bdbcd992ed515355239cb16114c0346a36f
Дмитрий Окулов <okulov.d@mail.ru>
2022-03-14 17:57:06 +0300

    Исправлены красные замечания первого ревью

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
-# Create your views here.
-from django.http import HttpResponse
 
-# Главная страница
 def index(request):
-    return HttpResponse('Главная')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    template = 'posts/index.html'
+    context = {
+        'posts': posts,
+    }
+    return render(request, template, context)
 
-def gruop_posts(request, slug):
-    return HttpResponse(f'Посты, отфильтрованные по группе {slug}')
 
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
